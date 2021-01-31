9156524300f169eb33c741a9383a194b6e09721d
aayushimalviya <aayushimalviya@users.noreply.github.com>
2021-01-31 18:11:36 +0530

    Java file added

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,24 @@
+package com.practice.arrays;
+
+public class FindDuplicateNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 2, 4, 5, 3, 5, 9, 0 };
+		findFirstDuplicateNum(arr);
+
+	}
+
+	private static void findFirstDuplicateNum(int[] arr) {
+		outer: for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					System.out.println("Duplicate number is " + arr[i]);
+					break outer;
+				}
+			}
+		}
+
+	}
+
+}
